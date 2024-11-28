5fea5824843c4e0b45344dd47cdee77875bdda35
Cris T. <netherstar05@gmail.com>
2024-11-28 18:46:43 +0800

    Finished Assignment
    It also includes the screenshots of results in Postman.

--- index.php
+++ index.php
@@ -6,14 +6,8 @@ header("Content-Type: application/json");
 //HTTP Verb(GET, POST, PUT, DELETE)
 $requestMethod = $_SERVER["REQUEST_METHOD"];
 
-//To hanlde if request doesn't exist
-$request = isset($_GET['id']) ? explode("/", trim($_GET['id'], "/")) : [];
-
-//How to handle HTTP Verb
-$requestMethod;
-
 //GET the task ID
-$task_id = isset($request[1]) ? intval($request[1]) : null;
+$task_id = isset($_GET['id']) ? intval($_GET['id']) : null;
 
     switch($requestMethod){
         case 'POST':
@@ -28,6 +22,24 @@ $task_id = isset($request[1]) ? intval($request[1]) : null;
             }
             
             break;
+        case 'DELETE':
+            if($task_id){
+                deleteTask($task_id);
+            }
+            else{
+                deleteAll();
+            }
+            break;
+        
+            case 'PUT':
+                if($task_id){
+                    updateTask($task_id);
+                }
+                else{
+                    echo json_encode(["message" => "Task ID is required."]);
+                    http_response_code(400);
+                }
+                break;
 
     default:
     http_response_code(405);
@@ -87,5 +99,93 @@ function getTask($id){
     else{
         echo json_encode(["message" => "Task not found."]);
     }
+}
+
+function deleteTask($id){
+    global $conn;
+
+    $sql = "DELETE FROM task WHERE id = $id";
+    $result = mysqli_query($conn, $sql);
+
+    if($result){
+        if(mysqli_affected_rows($conn) > 0){
+            echo json_encode(["message" => "Task deleted successfully."]);
+        }
+        else{
+            echo json_encode(["message" => "Task not found."]);
+        }
+    }
+    else{
+        echo json_encode(["message" => "Error deleting task: " . mysqli_error($conn)]);
+    }
+}
+
+function deleteAll(){
+    global $conn;
+
+    $sql = "DELETE FROM task";
+    $result = mysqli_query($conn, $sql);
+
+    if($result){
+        if(mysqli_affected_rows($conn) > 0){
+            echo json_encode(["message" => "All task has been deleted."]);
+        }
+        else{
+            echo json_encode(["message" => "Task not found."]);
+        }
+    }
+    else{
+        echo json_encode(["message" => "Error deleting task: " . mysqli_error($conn)]);
+    }
+}
+
+function updateTask($id){
+    global $conn;
+
+    $data = json_decode(file_get_contents("php://input"), true);
+
+    //Array variable to store missing fields in the PUT request
+    $missingFields = [];
+
+    //Checks each fields that are required in the PUT request. If the user didn't include it, it will store in the $missingFields
+    if (empty($data['title'])) {
+        $missingFields[] = 'title';
+    }
+    elseif (empty($data['description'])) {
+        $missingFields[] = 'description';
+    }
+    elseif (empty($data['status'])) {
+        $missingFields[] = 'status';
+    }
+
+    //If the $missingFields is not empty, it will tell the user in Postman that they have missing field/s that they need to include in their PUT request
+    if (!empty($missingFields)) {
+        $missingFieldsList = implode(', ', $missingFields); //The implode function separates one whole string into arrays or key-value pairs
+        echo json_encode(["message" => "Missing required fields: $missingFieldsList"]); //Tells the user the missing field/s that need to be included in the PUT request
+        http_response_code(400); // Bad request
+        return;
+    }
+
+    //Data that are being updated in the record
+    $title = $data['title'];
+    $description = $data['description'];
+    $status = $data['status'];
+
+    //SQL query to update the entire task
+    $sql = "UPDATE task SET title = '$title', description = '$description', status = '$status' WHERE id = $id";
+    $result = mysqli_query($conn, $sql);
+
+    if($result){
+        if(mysqli_affected_rows($conn) > 0){
+            echo json_encode(["message" => "Task updated successfully."]);
+        }
+        else{
+            echo json_encode(["message" => "Task not found."]);
+        }
+    }
+    else{
+        echo json_encode(["message" => "Error updating task: " . mysqli_error($conn)]);
+    }
+
 }
 ?>
